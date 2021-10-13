55613adee78ae9dd099f0f10df805afe2e7cfe58
cj.green <cj.green@intimetec.com>
2021-10-13 14:06:26 -0600

    required added to model

--- OdeToFood.data/IRestaurantData.cs
+++ OdeToFood.data/IRestaurantData.cs
@@ -10,6 +10,8 @@ namespace OdeToFood.data
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        int Commit();
     }
     public class InMemoryRestaurantData : IRestaurantData
     { 
@@ -27,9 +29,23 @@ namespace OdeToFood.data
 
        public Restaurant GetById(int id)
         {
-            return restaurants.SingleOrDefault(r => r.Id == id);        }
-
-
+            return restaurants.SingleOrDefault(r => r.Id == id);       
+        }
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+                if(restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+        public int Commit()
+        {
+            return 0;
+        }
         public IEnumerable<Restaurant> GetRestaurantsByName(string name)
         {
             return from r in restaurants
